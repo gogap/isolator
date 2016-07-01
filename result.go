ac4b3f389d11ab20a173c6f34f9cf79a3885ef6a
Zeal <xujinzheng@gmail.com>
2016-07-01 11:12:25 +0800

    invoke result end could input an nil func

--- result.go
+++ result.go
@@ -12,25 +12,6 @@ func (p Result) End(fn interface{}) error {
 }
 
 func mapTo(fn interface{}, values []reflect.Value) (err error) {
-	fnType := reflect.TypeOf(fn)
-	if fnType.Kind() != reflect.Func {
-		panic(errors.New("result mapper func should be func"))
-	}
-
-	if fnType.NumIn() != len(values) {
-
-		if len(values) != 1 {
-			panic(errors.New("mapper func args number error"))
-		}
-
-		newV := []reflect.Value{}
-		for i := 0; i < fnType.NumIn()-1; i++ {
-			newV = append(newV, reflect.Zero(fnType.In(i)))
-		}
-
-		newV = append(newV, values[0])
-		values = newV
-	}
 
 	if len(values) > 0 {
 		lastV := values[len(values)-1]
@@ -39,9 +20,30 @@ func mapTo(fn interface{}, values []reflect.Value) (err error) {
 		}
 	}
 
-	fnValue := reflect.ValueOf(fn)
+	if fn != nil {
+		fnType := reflect.TypeOf(fn)
+		if fnType.Kind() != reflect.Func {
+			panic(errors.New("result mapper func should be func"))
+		}
 
-	fnValue.Call(values)
+		if fnType.NumIn() != len(values) {
+
+			if len(values) != 1 {
+				panic(errors.New("mapper func args number error"))
+			}
+
+			newV := []reflect.Value{}
+			for i := 0; i < fnType.NumIn()-1; i++ {
+				newV = append(newV, reflect.Zero(fnType.In(i)))
+			}
+
+			newV = append(newV, values[0])
+			values = newV
+		}
+
+		fnValue := reflect.ValueOf(fn)
+		fnValue.Call(values)
+	}
 
 	return
 }
